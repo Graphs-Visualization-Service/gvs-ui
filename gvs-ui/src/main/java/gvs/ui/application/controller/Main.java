1ac1b54cbda31118339ddc03ab8fd9c9a37ef858
Michael Wieland <michi.wieland@hotmail.com>
2017-09-28 15:39:36 +0200

    62: Basic structure added - Gradle project setup - JUnit (main/test) - Log4J and Logback

---
+++ gvs-ui/src/main/java/gvs/ui/application/controller/Main.java
@@ -0,0 +1,49 @@
+package gvs.ui.application.controller;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("GVS");
+
+		initRootLayout();
+	}
+
+	private void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("../view/Application.fxml"));
+			rootLayout = (AnchorPane) loader.load();
+			
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			// TODO add logging framework
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ gvs-ui/src/main/java/gvs/ui/application/model/ApplicationModel.java
@@ -0,0 +1,14 @@
+package gvs.ui.application.model;
+
+import java.util.Observable;
+
+public class ApplicationModel extends Observable {
+
+	// private ObservableList<ISessionModel> sessions;
+
+	public ApplicationModel() {
+		// TODO create classes
+		// this.sessions = FXCollections.observableArrayList()
+	}
+
+}

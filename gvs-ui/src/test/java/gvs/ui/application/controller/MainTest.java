63eaaef16858bdf2be77c9011e5b876796d336af
Michael Wieland <michi.wieland@hotmail.com>
2017-09-28 15:44:25 +0200

    Testclass added

---
+++ gvs-ui/src/test/java/gvs/ui/application/controller/MainTest.java
@@ -0,0 +1,14 @@
+package gvs.ui.application.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

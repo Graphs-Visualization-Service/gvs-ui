7ac64e76a746dcfad53240f7b3f117fc523d284e
Murièle Trentini <muriele@trentini.ch>
2017-09-29 15:30:23 +0200

    add gvslauncher. remove corba server section

---
+++ src/main/java/gvs/GVSLauncher.java
@@ -0,0 +1,28 @@
+package gvs;
+
+import gvs.common.Configuration;
+import gvs.server.socket.SocketServer;
+import gvs.ui.application.view.ApplicationView;
+
+/**
+ * Starts the Graphs-Visualization Service
+ * 
+ * @author aegli
+ *
+ */
+public class GVSLauncher {
+
+	public static void main(String[] args) {
+
+		ApplicationView av = new ApplicationView();
+
+		av.setVisible(true);
+		try {
+			SocketServer server = new SocketServer();
+			server.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}

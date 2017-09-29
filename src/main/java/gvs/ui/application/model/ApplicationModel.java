f8c8a11372d0c153d4ead6e935bd106dd2b1cb56
Murièle Trentini <muriele@trentini.ch>
2017-09-29 16:39:03 +0200

    add ui.application.model. spellcheck docs

---
+++ src/main/java/gvs/ui/application/model/ApplicationModel.java
@@ -0,0 +1,44 @@
+package gvs.ui.application.model;
+
+import gvs.interfaces.ISessionController;
+
+import java.util.Observable;
+
+/**
+ * Holds the model of the current session
+ * 
+ * @author aegli
+ *
+ */
+public class ApplicationModel extends Observable {
+
+	private ISessionController sessionController = null;
+
+	/**
+	 * Builds an instance of the application model
+	 *
+	 */
+	public ApplicationModel() {
+
+	}
+
+	/**
+	 * Sets currently requested session
+	 * 
+	 * @param sc
+	 */
+	public void setSession(ISessionController sc) {
+		this.sessionController = sc;
+		setChanged();
+		notifyObservers();
+	}
+
+	/**
+	 * Returns current session, which is held by the model
+	 * 
+	 * @return
+	 */
+	public ISessionController getSession() {
+		return sessionController;
+	}
+}

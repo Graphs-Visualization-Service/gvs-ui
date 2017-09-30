8713207be8addd7d8bd28526d4243a4ce8c8b8e8
Murièle Trentini <muriele@trentini.ch>
2017-09-30 10:32:18 +0200

    add ui.tree.model.VisualizationTreeModel. spellcheck docs

---
+++ src/main/java/gvs/ui/tree/model/VisualizationTreeModel.java
@@ -0,0 +1,43 @@
+package gvs.ui.tree.model;
+
+import gvs.ui.tree.layout.TreeLayoutController;
+
+import java.util.Observable;
+
+/**
+ * Model which gives updates to the visualization panel
+ * 
+ * @author aegli
+ *
+ */
+
+public class VisualizationTreeModel extends Observable {
+
+	private TreeModel treeModel = null;
+
+	/**
+	 * Builds an instance of a VisualTreeModel
+	 *
+	 */
+	public VisualizationTreeModel() {
+	}
+
+	/**
+	 * Sets a tree model for displaying
+	 * 
+	 * @param pTreeModel
+	 */
+	public void setTreeModel(TreeModel pTreeModel, TreeLayoutController pLayoutController) {
+		this.treeModel = pTreeModel;
+		setChanged();
+		notifyObservers(pLayoutController);
+	}
+
+	/**
+	 * Returns currently held tree model
+	 * 
+	 */
+	public TreeModel getTreeModel() {
+		return treeModel;
+	}
+}

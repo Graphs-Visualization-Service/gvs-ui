b312e535baa40cb504b220240c902d35c4627f24
Murièle Trentini <muriele@trentini.ch>
2017-09-30 10:44:11 +0200

    add application controller classes. replace logger. spellcheck docs.

---
+++ src/main/java/gvs/ui/application/controller/ApplicationController.java
@@ -0,0 +1,224 @@
+package gvs.ui.application.controller;
+
+import gvs.common.Persistor;
+import gvs.interfaces.ISessionController;
+import gvs.interfaces.ITreeSessionController;
+import gvs.ui.application.model.ApplicationModel;
+import gvs.ui.graph.controller.GraphSessionController;
+import gvs.ui.graph.model.GraphModel;
+import gvs.ui.tree.controller.TreeSessionController;
+import gvs.ui.tree.model.TreeModel;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * The Application Controller reacts to events from the user or newly reveived
+ * data. The requested operations will be executed.
+ * 
+ * @author aegli
+ *
+ */
+public class ApplicationController {
+	private Logger appContLogger = null;
+	private Vector<ISessionController> sessionControllers = null;
+	private static ApplicationController appController = null;
+	private ApplicationModel applicationModel = null;
+	private boolean layoutOption = false;
+
+	ISessionController duplicatedSessions = null;
+	ISessionController deletingSession = null;
+	ISessionController defaultGraphSession = null;
+
+	private ApplicationController(ApplicationModel am) {
+		this.applicationModel = am;
+		this.sessionControllers = new Vector<ISessionController>();
+		// TODO: check logger replacement
+		// this.appContLogger =
+		// gvs.common.Logger.getInstance().getApplciationControllerLogger();
+		this.appContLogger = LoggerFactory.getLogger(ApplicationController.class);
+	}
+
+	public synchronized static ApplicationController getInstance(ApplicationModel pApplicationModel) {
+		if (appController == null) {
+			appController = new ApplicationController(pApplicationModel);
+		}
+		return appController;
+	}
+
+	/**
+	 * Returns an instance of the application controller
+	 */
+	public synchronized static ApplicationController getInstance() {
+		return appController;
+	}
+
+	/**
+	 * Sets layout option for layout engine: hard or soft layout.
+	 * 
+	 * @param pLayoutOption
+	 */
+	public void setLayoutOption(boolean pLayoutOption) {
+		this.layoutOption = pLayoutOption;
+	}
+
+	/**
+	 * Returns layout option defined by user
+	 * 
+	 * @return
+	 */
+	public boolean getLayoutOption() {
+		return layoutOption;
+	}
+
+	/**
+	 * Sets session chosen from combobox, informs model and updates view
+	 * 
+	 * @param pSessionController
+	 */
+	public void setComboSession(ISessionController pSessionController) {
+		applicationModel.setSession(pSessionController);
+	}
+
+	/**
+	 * Returns available session for displaying in combobox
+	 * 
+	 * @return
+	 */
+	public Vector getSessionContoller() {
+		return sessionControllers;
+	}
+
+	/**
+	 * Loads a session from a specific file
+	 * 
+	 * @param fileName
+	 * @param persistor
+	 */
+	public void setRequestedFile(String fileName, Persistor persistor) {
+		appContLogger.info("Load session from directory");
+		ISessionController loadedSession = (ISessionController) persistor.loadFile(fileName);
+		long loadedSessionId = loadedSession.getSessionId();
+		ISessionController tempSession = null;
+
+		Iterator it = sessionControllers.iterator();
+		while (it.hasNext()) {
+			tempSession = ((ISessionController) it.next());
+
+			if (tempSession.getSessionId() == loadedSessionId) {
+				duplicatedSessions = tempSession;
+
+			}
+		}
+
+		if (duplicatedSessions != null) {
+			appContLogger.warn("Duplicated session detected while loading. Delete duplicated session");
+			sessionControllers.remove(duplicatedSessions);
+			duplicatedSessions = null;
+		}
+
+		this.sessionControllers.add(loadedSession);
+		applicationModel.setSession(((ISessionController) sessionControllers.lastElement()));
+	}
+
+	/**
+	 * Deletes a chosen session
+	 * 
+	 * @param pSessionController
+	 */
+	public void deleteSession(ISessionController pSessionController) {
+		appContLogger.info("Deleting graph session");
+		long deleteSessionId = pSessionController.getSessionId();
+		ISessionController temp = null;
+
+		Iterator it = sessionControllers.iterator();
+		while (it.hasNext()) {
+			temp = ((ISessionController) it.next());
+
+			if (temp.getSessionId() == deleteSessionId) {
+				deletingSession = temp;
+			}
+		}
+
+		sessionControllers.remove(deletingSession);
+		if (sessionControllers.size() > 0) {
+			appContLogger.debug("Set former graph session");
+			applicationModel.setSession(((ISessionController) sessionControllers.firstElement()));
+
+		} else {
+			appContLogger.debug("Set empty graph session");
+			defaultGraphSession = new GraphSessionController();
+			applicationModel.setSession(defaultGraphSession);
+		}
+	}
+
+	/**
+	 * Adds a new tree model, if an associated session exists, adds model to
+	 * session. Otherwise, creates a new tree session
+	 * 
+	 * @param pTreeModel
+	 * @param pId
+	 * @param pSessionName
+	 */
+	public synchronized void addTreeModel(TreeModel pTreeModel, long pId, String pSessionName) {
+		appContLogger.info("New Tree arrived");
+		try {
+			Monitor.getInstance().lock();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Iterator sessionIt = sessionControllers.iterator();
+		boolean isSessionExisting = false;
+		while (sessionIt.hasNext()) {
+			ISessionController sc = (ISessionController) (sessionIt.next());
+			if (sc.getSessionId() == pId) {
+				appContLogger.debug("Add tree to exsting session");
+				((ITreeSessionController) sc).addTreeModel(pTreeModel);
+				isSessionExisting = true;
+			}
+		}
+		if (isSessionExisting == false) {
+			appContLogger.debug("Build new tree session");
+			ITreeSessionController newSession = new TreeSessionController(pId, pSessionName, pTreeModel);
+			sessionControllers.add(newSession);
+			appContLogger.debug("Set session as actual model");
+			applicationModel.setSession(newSession);
+		}
+		Monitor.getInstance().unlock();
+	}
+
+	/**
+	 * Adds a new graph model, if an associated session exists, adds model to
+	 * session. Otherwise, creates a new graph session
+	 * 
+	 * @param pGraphModel
+	 * @param pId
+	 * @param pSessionName
+	 */
+	public synchronized void addModel(GraphModel pGraphModel, long pId, String pSessionName) {
+		appContLogger.info("New graph arrived");
+		Iterator sessionIt = sessionControllers.iterator();
+		boolean isSessionExisting = false;
+		while (sessionIt.hasNext()) {
+			ISessionController sc = (ISessionController) (sessionIt.next());
+			if (sc.getSessionId() == pId) {
+				appContLogger.debug("Add graph to exsting session");
+				((GraphSessionController) sc).addGraphModel(pGraphModel);
+
+				isSessionExisting = true;
+			}
+		}
+		if (isSessionExisting == false) {
+			appContLogger.debug("Build new graph session");
+			GraphSessionController newSession = new GraphSessionController(pId, pSessionName, pGraphModel);
+			sessionControllers.add(newSession);
+			appContLogger.debug("Set session as actual model");
+			applicationModel.setSession(newSession);
+		}
+	}
+}

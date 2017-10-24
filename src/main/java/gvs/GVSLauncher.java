510e9705de565b5f8191fafa8b0529f31cfbfb2a
Michael Wieland <michi.wieland@hotmail.com>
2017-10-24 10:59:12 +0200

    129: Open the gvs application in the main thread

--- src/main/java/gvs/GVSLauncher.java
+++ src/main/java/gvs/GVSLauncher.java
@@ -1,31 +1,39 @@
 package gvs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import gvs.server.socket.SocketServer;
 import gvs.ui.application.controller.GVSApplication;
-import gvs.ui.application.view.ApplicationView;
 
 /**
  * Starts the Graphs-Visualization Service.
  * 
- * @author aegli
+ * @author mwieland
  */
 public class GVSLauncher {
 
+  private static final Logger logger = LoggerFactory
+      .getLogger(GVSLauncher.class);
+
   /**
-   * Main method. 
-   * @param args console arguments
+   * Main method.
+   * 
+   * @param args
+   *          console arguments
    */
   public static void main(String[] args) {
-//    GVSApplication.launch(GVSApplication.class);
-    new Thread(() ->  GVSApplication.launch(GVSApplication.class)).start();
-    ApplicationView av = new ApplicationView();
-    av.setVisible(true);
     try {
+      logger.info("Start GVS UI 2.0...");
+      GVSApplication.launch(GVSApplication.class);
+      logger.info("GVS UI started.s");
+
+      logger.info("Start Server Socket...");
       SocketServer server = new SocketServer();
       server.start();
+      logger.info("Sever Socket started.");
     } catch (Exception e) {
-      e.printStackTrace();
+      logger.error("Unable to start GVS UI", e);
     }
-
   }
 }

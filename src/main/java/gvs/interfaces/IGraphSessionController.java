d9ec43c55c58e98280623879a07629cb763e2cc8
Michael Wieland <michi.wieland@hotmail.com>
2017-11-06 17:25:48 +0100

    150: removed unused observer interface. was used to update the view after layout process has finished

--- src/main/java/gvs/interfaces/IGraphSessionController.java
+++ src/main/java/gvs/interfaces/IGraphSessionController.java
@@ -1,15 +1,12 @@
 package gvs.interfaces;
 
 import java.util.List;
-import java.util.Observable;
 
 import gvs.business.model.graph.Graph;
 
 public interface IGraphSessionController extends ISessionController {
 
   void addGraph(Graph graph);
-
-  void update(Observable o, Object arg);
 
   Graph getCurrentGraph();
 

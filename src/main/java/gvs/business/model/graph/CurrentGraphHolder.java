ee4ad8fef679fca42c185f5d7bf48e9f63ac623f
Murièle Trentini <muriele@trentini.ch>
2017-11-07 16:38:48 +0100

    144: add logger

--- src/main/java/gvs/business/model/graph/CurrentGraphHolder.java
+++ src/main/java/gvs/business/model/graph/CurrentGraphHolder.java
@@ -2,14 +2,24 @@ package gvs.business.model.graph;
 
 import java.util.Observable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.inject.Singleton;
+
+
+import gvs.business.logic.graph.GraphSessionController;
 
 @Singleton
 public class CurrentGraphHolder extends Observable {
 
   private Graph currentGraph;
+
+  private static final Logger logger = LoggerFactory
+      .getLogger(CurrentGraphHolder.class);
   
   public void setCurrentGraph(Graph newGraph) {
+    logger.info("Setting current graph and notifying observers.");
     this.currentGraph = newGraph;
     setChanged();
     notifyObservers();

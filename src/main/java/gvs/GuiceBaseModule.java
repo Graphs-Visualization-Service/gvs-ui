eeaa58dd41dcc6d6e44b23b24cab84f8fbcbdf30
Michael Wieland <michi.wieland@hotmail.com>
2017-11-13 17:05:12 +0100

    156: Renamed GraphSessionFactory in SessionFactory Removed Graph Parameter, as new graphs should be added with the addGraph() method

--- src/main/java/gvs/GuiceBaseModule.java
+++ src/main/java/gvs/GuiceBaseModule.java
@@ -5,7 +5,7 @@ import com.google.inject.assistedinject.FactoryModuleBuilder;
 
 import gvs.access.ClientConnectionFactory;
 import gvs.access.XmlReaderFactory;
-import gvs.business.logic.graph.GraphSessionFactory;
+import gvs.business.logic.graph.SessionFactory;
 import gvs.business.logic.graph.GraphSessionReplayFactory;
 import gvs.business.logic.physics.ticker.AreaTickerFactory;
 
@@ -20,8 +20,7 @@ public class GuiceBaseModule extends AbstractModule {
   @Override
   protected void configure() {
     install(new FactoryModuleBuilder().build(ClientConnectionFactory.class));
-    install(
-        new FactoryModuleBuilder().build(GraphSessionFactory.class));
+    install(new FactoryModuleBuilder().build(SessionFactory.class));
     install(new FactoryModuleBuilder().build(GraphSessionReplayFactory.class));
     install(new FactoryModuleBuilder().build(XmlReaderFactory.class));
     install(new FactoryModuleBuilder().build(AreaTickerFactory.class));

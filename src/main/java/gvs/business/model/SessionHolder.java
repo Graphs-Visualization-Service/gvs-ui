dfacc59c8a11e2c9579e541562863a376f51fabb
Michael Wieland <michi.wieland@hotmail.com>
2017-11-13 17:19:13 +0100

    156: add logger to session holder

--- src/main/java/gvs/business/model/SessionHolder.java
+++ src/main/java/gvs/business/model/SessionHolder.java
@@ -4,6 +4,9 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Observable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -21,6 +24,9 @@ public class SessionHolder extends Observable {
   private ISession currentSession = null;
   private final Collection<ISession> sessions;
 
+  private static final Logger logger = LoggerFactory
+      .getLogger(SessionHolder.class);
+
   @Inject
   public SessionHolder() {
     this.sessions = new HashSet<>();
@@ -32,6 +38,8 @@ public class SessionHolder extends Observable {
    * @param newSession
    */
   public synchronized void setCurrentSession(ISession newSession) {
+    logger.info("Setting current session and notify observers.");
+
     this.currentSession = newSession;
     this.currentSession.changeCurrentGraphToFirst();
     setChanged();

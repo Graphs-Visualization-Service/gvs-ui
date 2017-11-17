42fe2ebfc101032fa496a629ed60cc5378cb4b89
Michael Wieland <michi.wieland@hotmail.com>
2017-11-17 08:28:52 +0100

    176: removed unused import set application icon

--- src/main/java/gvs/GVSApplication.java
+++ src/main/java/gvs/GVSApplication.java
@@ -15,6 +15,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -84,6 +85,8 @@ public class GVSApplication extends Application {
     primaryStage.setMinHeight(MIN_HEIGHT);
     primaryStage.setMinWidth(MIN_WIDTH);
     primaryStage.setTitle(APP_NAME);
+    primaryStage.getIcons().add(new Image(
+        getClass().getClassLoader().getResourceAsStream("images/logo.png")));
 
     primaryStage.setOnCloseRequest(e -> {
       Platform.exit();

45ceb52a6f5361ef28cc48a00132b41546927670
Murièle Trentini <muriele@trentini.ch>
2017-11-23 11:42:51 +0100

    195: add binaryvertex class

---
+++ src/main/java/gvs/business/model/tree/BinaryTreeVertex.java
@@ -0,0 +1,29 @@
+package gvs.business.model.tree;
+
+import gvs.business.model.styles.GVSStyle;
+import gvs.util.FontAwesome.Glyph;
+
+public class BinaryTreeVertex extends TreeVertex {
+
+  public BinaryTreeVertex(long id, String label, GVSStyle style,
+      boolean isUserPositioned, Glyph icon) {
+    super(id, label, style, isUserPositioned, icon);
+  }
+  
+  public TreeVertex getLeftChild() {
+    return getChildren().get(0);
+  }
+  
+  public TreeVertex getRightChild() {
+    return getChildren().get(1);
+  }
+  
+  public void setLeftChild(TreeVertex child) {
+    getChildren().set(0,child);
+  }
+  
+  public void setRightChild(TreeVertex child) {
+    getChildren().set(1,child);
+  }
+
+}

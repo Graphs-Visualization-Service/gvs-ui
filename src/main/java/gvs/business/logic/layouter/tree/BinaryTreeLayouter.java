ec0c2fefcf186abacc4e8e72cb3230a6d5304b52
Murièle Trentini <muriele@trentini.ch>
2017-11-24 09:37:51 +0100

    195: next step in algorithm

--- src/main/java/gvs/business/logic/layouter/tree/BinaryTreeLayouter.java
+++ src/main/java/gvs/business/logic/layouter/tree/BinaryTreeLayouter.java
@@ -1,9 +1,17 @@
 package gvs.business.logic.layouter.tree;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.google.inject.Singleton;
 
 import gvs.business.logic.layouter.ILayouter;
 import gvs.business.model.Graph;
+import gvs.business.model.tree.BinaryTreeVertex;
+import gvs.business.model.tree.TreeVertex;
 import gvs.util.Action;
 
 /**
@@ -15,10 +23,52 @@ import gvs.util.Action;
  */
 @Singleton
 public class BinaryTreeLayouter implements ILayouter {
+  private static final int NODESIZE = 50; // TODO: account for nodeSize
+  private static final double SIBLING_DISTANCE = 20;
+  private static final double LEVEL_DISTANCE = 20;
 
   @Override
   public void layoutGraph(Graph currentGraph, boolean useRandomLayout,
       Action callback) {
+    List<BinaryTreeVertex> roots = currentGraph.getVertices().stream()
+        .map(v -> (BinaryTreeVertex) v).filter(v -> v.isRoot())
+        .collect(Collectors.toList());
+
+    // TODO: support multiple roots
+    roots.forEach(root -> {
+      setup(root, 0, null, null);
+      addMods(root);
+    });
+  }
+
+  private void setup(BinaryTreeVertex vertex, int depth,
+      Map<Integer, Integer> nexts, Map<Integer, Double> offset) {
+    if (nexts == null) {
+      nexts = new HashMap<>();
+    }
+    if (offset == null) {
+      offset = new HashMap<>();
+    }
+    List<BinaryTreeVertex> children = vertex.getChildren().stream()
+        .map(c -> (BinaryTreeVertex) c).collect(Collectors.toList());
+    for (BinaryTreeVertex child : children) {
+      setup(child, depth + 1, nexts, offset);
+    }
+    vertex.setYPosition(depth * LEVEL_DISTANCE + LEVEL_DISTANCE);
+
+    int place = 0;
+    if (children.isEmpty()) {
+      Integer next = nexts.get(depth);
+      place = next == null ? 0 : next;
+      vertex.setXPosition(place);
+    } else if (children.size() == 1) {
+      BinaryTreeVertex leftChild = (BinaryTreeVertex) vertex.getLeftChild();
+      BinaryTreeVertex rightChild = (BinaryTreeVertex) vertex.getRightChild();
+    }
+
+  }
+
+  private void addMods(TreeVertex root) {
     // TODO Auto-generated method stub
 
   }
--- src/main/java/gvs/business/model/tree/BinaryTreeVertex.java
+++ src/main/java/gvs/business/model/tree/BinaryTreeVertex.java
@@ -4,6 +4,8 @@ import gvs.business.model.styles.GVSStyle;
 import gvs.util.FontAwesome.Glyph;
 
 public class BinaryTreeVertex extends TreeVertex {
+  private boolean hasLeftChild;
+  private boolean hasRightChild;
 
   public BinaryTreeVertex(long id, String label, GVSStyle style,
       boolean isUserPositioned, Glyph icon) {
@@ -11,19 +13,43 @@ public class BinaryTreeVertex extends TreeVertex {
   }
 
   public TreeVertex getLeftChild() {
-    return getChildren().get(0);
+    if (hasLeftChild) {
+      return getChildren().get(0);
+    }
+    return null;
   }
 
   public TreeVertex getRightChild() {
-    return getChildren().get(1);
+    if (hasRightChild) {
+      return getChildren().get(1);
+    }
+    return null;
   }
 
   public void setLeftChild(TreeVertex child) {
     getChildren().set(0, child);
+    hasLeftChild = true;
   }
 
   public void setRightChild(TreeVertex child) {
     getChildren().set(1, child);
+    hasRightChild = true;
+  }
+
+  public boolean isLeftChild() {
+    TreeVertex parent = getParent();
+    if (parent != null) {
+      return parent.getChildren().indexOf(this) == 0;
+    }
+    return false;
+  }
+
+  public boolean isRightChild() {
+    TreeVertex parent = getParent();
+    if (parent != null) {
+      return parent.getChildren().indexOf(this) == 1;
+    }
+    return false;
   }
 
 }
